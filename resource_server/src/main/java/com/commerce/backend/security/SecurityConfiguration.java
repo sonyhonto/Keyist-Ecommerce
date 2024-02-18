6bd917ad309102ea1b63ea4964babf238408628f
sonyhonto <157215626+sonyhonto@users.noreply.github.com>
2024-02-18 13:39:28 +0100

    disable security

--- resource_server/src/main/java/com/commerce/backend/security/SecurityConfiguration.java
+++ resource_server/src/main/java/com/commerce/backend/security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.commerce.backend.security;
 
-import com.commerce.backend.constants.SecurityConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 
+import com.commerce.backend.constants.SecurityConstants;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -29,25 +29,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 securityConstants.getConnectionTimeout(),
                 securityConstants.getReadTimeout(),
                 securityConstants.getAuthUrl(),
-                securityConstants.getAuthUsername()
-        );
+                securityConstants.getAuthUsername());
     }
 
     @Override
-    public void configure(final HttpSecurity http) throws Exception {
-        http.cors().and()
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(securityConstants.getWhitelist()).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .opaqueToken(opaqueToken -> opaqueToken
-                                .introspector(introspector())
-                        )
-                );
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests(requests -> requests
+                        .anyRequest().permitAll());
+
+        // .cors().and()
+        // .csrf().disable()
+        // .authorizeRequests()
+        // .antMatchers(securityConstants.getWhitelist()).permitAll()
+        // .anyRequest().authenticated()
+        // .and()
+        // .sessionManagement()
+        // .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        // .and()
+        // .oauth2ResourceServer(oauth2 -> oauth2
+        // .opaqueToken(opaqueToken -> opaqueToken
+        // .introspector(introspector())
+        // )
+        // );
     }
 }
