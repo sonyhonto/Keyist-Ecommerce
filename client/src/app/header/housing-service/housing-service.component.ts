cbc2216d5dc9873067a0af3285a94967a059aa70
sonyhonto <157215626+sonyhonto@users.noreply.github.com>
2024-02-24 12:02:08 +0100

    seller component integrated

--- client/src/app/header/housing-service/housing-service.component.ts
+++ client/src/app/header/housing-service/housing-service.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,inject , OnInit } from '@angular/core';
+import { Seller } from 'src/app/store/model';
+import { SellerService } from 'src/app/edit/seller.service'; 
 
 @Component({
   selector: 'app-housing-service',
   templateUrl: './housing-service.component.html',
-  styleUrls: ['./housing-service.component.scss']
+  styleUrls: ['./housing-service.component.scss'],
 })
 export class HousingServiceComponent implements OnInit {
 
-  constructor() { }
+  sellerList: Seller[] = [];
+  filteredSellerList: Seller[] = [];
+
+  constructor(private sellerService: SellerService) {
+  }
 
   ngOnInit(): void {
+    this.sellerList = this.sellerService.getAllSellers();
+    this.filteredSellerList = this.sellerList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredSellerList = this.sellerList;
+      return;
+    }
+
+    this.filteredSellerList = this.sellerList.filter(
+      seller => seller?.name.toLowerCase().includes(text.toLowerCase())
+    );
   }
 
 }
